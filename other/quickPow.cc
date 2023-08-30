81e1e28968387f184093fc7fe968ba2632ff9527
daskisnow <437207609@qq.com>
2023-08-30 21:12:16 +0800

    修改

--- other/quickPow.cc
+++ other/quickPow.cc
@@ -2,21 +2,26 @@
 
 using namespace std;
 
-// 加速幂运算(底数x, 指数n, x的n次幂)
-int qpow(int x, int n)
-{
-    int res = 1;
+// 加速幂运算(底数x, 指数n, x的n次幂), Leetcode 50. Pow(x, n)
+double myPow(double x, long long n) {
+    if(x == 0) return 0;
+    if(x == 1) return 1;
+    if(n < 0) 
+    {
+        x = 1.0 / x;
+        n = abs(n);
+    }
+    double res = 1.0;
     for(; n; n >>= 1, x *= x)
     {
-        (n & 1) && (res *= x);  // 相当于if(n & 1) res *= x;
+        (n & 1) && (res *= x);
     }
     return res;
 }
 
-
 int main(int argc, char *argv[])
 {
-    cout << qpow(2, 10) << endl;
+    cout << myPow(-2, 11) << endl;
     return 0;
 }
 
